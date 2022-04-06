5849b33bd18f4fb3dd72a8a1e0279f988dacffc3
Sang-hyun, Cho <cho911115@gmail.com>
2022-04-06 15:23:13 +0900

    문제 있는 코드 작성 1

--- src/main/java/com/choshsh/sonar/sonar/api/HelloController.java
+++ src/main/java/com/choshsh/sonar/sonar/api/HelloController.java
@@ -1,27 +1,38 @@
 package com.choshsh.sonar.sonar.api;
 
 import lombok.Data;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    Flux<String> hello() {
-        return Flux.just("Hello", "World");
+    Mono<String> hello() {
+        return Mono.just("Hello World");
     }
 
     @GetMapping("/user")
-    ResponseEntity<Person> test() {
+    Mono<ResponseEntity<Person>> test() {
+        String foodSecond;
+        try {
+            foodSecond = "피자";
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
         Person person = new Person();
         person.setName("ethan");
         person.setFavoriteFood("햄버거");
 
-        return new ResponseEntity<>(person, null, HttpStatus.OK);
+        return Mono.just(
+                ResponseEntity
+                        .status(200)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(person));
     }
 
     @Data
